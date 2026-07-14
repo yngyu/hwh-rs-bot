5b9cf905226cfbfe15146248665f25cdbcd90213
Yoshinari Gyu <50069930+yngyu@users.noreply.github.com>
2026-07-15 02:43:37 +0900

    remove remind

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,5 @@
-use futures::TryStreamExt;
 use mongodb::{
-    bson::{self, doc, Document},
+    bson::{doc, Document},
     Client, Collection,
 };
 
@@ -10,7 +9,6 @@ use crate::Error;
 
 pub struct Db {
     speaker_coll: Collection<Document>,
-    remind_coll: Collection<Document>,
 }
 
 pub async fn build_db() -> Result<Db, Error> {
@@ -18,12 +16,8 @@ pub async fn build_db() -> Result<Db, Error> {
     let client = Client::with_uri_str(uri).await?;
     let database = client.database("bot");
     let speaker_coll = database.collection("speakers");
-    let remind_coll = database.collection("reminds");
 
-    Ok(Db {
-        speaker_coll,
-        remind_coll,
-    })
+    Ok(Db { speaker_coll })
 }
 
 impl Db {
@@ -47,65 +41,5 @@ impl Db {
             .await?;
 
         Ok(())
-    }
-
-    pub async fn add_new_reminder(
-        &self,
-        user_id: u64,
-        channel_id: u64,
-        content: String,
-        remind_at: chrono::DateTime<chrono::Utc>,
-    ) -> Result<(), Error> {
-        let remind_at = bson::DateTime::parse_rfc3339_str(remind_at.to_rfc3339())?;
-
-        let reminder = doc! {
-            "user_id": user_id.to_string(),
-            "channel_id": channel_id.to_string(),
-            "content": content,
-            "remind_at": remind_at,
-        };
-
-        self.remind_coll.insert_one(reminder).await?;
-
-        Ok(())
-    }
-
-    pub async fn get_reminders(&self) -> Result<Vec<Document>, Error> {
-        let begin = chrono::Utc::now() - chrono::Duration::minutes(1);
-        let begin = bson::DateTime::parse_rfc3339_str(begin.to_rfc3339())?;
-
-        let end = bson::DateTime::now();
-
-        let filter = doc! {
-            "remind_at": {
-                "$gte": begin,
-                "$lte": end
-            }
-        };
-        let cursor = self.remind_coll.find(filter).await?;
-        let reminds: Vec<Document> = cursor.try_collect::<Vec<Document>>().await?;
-
-        Ok(reminds)
-    }
-
-    pub async fn remove_reminder(&self, document: Document) -> Result<(), Error> {
-        let id = document.get_object_id("_id")?;
-        self.remind_coll.delete_one(doc! { "_id": id }).await?;
-
-        Ok(())
-    }
-
-    pub async fn remove_old_reminders(&self) -> Result<Vec<Document>, Error> {
-        let before = chrono::Utc::now() - chrono::Duration::minutes(1);
-        let before = bson::DateTime::parse_rfc3339_str(before.to_rfc3339())?;
-
-        let filter = doc! { "remind_at": { "$lt": before } };
-
-        let cursor = self.remind_coll.find(filter.clone()).await?;
-        let reminds: Vec<Document> = cursor.try_collect::<Vec<Document>>().await?;
-
-        self.remind_coll.delete_many(filter).await?;
-
-        Ok(reminds)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![warn(clippy::str_to_string)]
 
 mod db;
-mod remind;
 mod voice;
 
 use poise::serenity_prelude::{self as serenity};
@@ -17,7 +16,6 @@ type Context<'a> = poise::Context<'a, Data, Error>;
 // Custom user data passed to all command functions
 pub struct Data {
     voice: voice::Voice,
-    remind: remind::Remind,
 }
 
 async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
@@ -52,7 +50,6 @@ async fn main() {
             voice::disconnect_vc(),
             voice::show_vc(),
             voice::set_vc(),
-            remind::remind(),
         ],
         // The global error handler for all error cases that may occur
         on_error: |error| Box::pin(on_error(error)),
@@ -71,7 +68,6 @@ async fn main() {
                 Ok(Data {
                     voice: build_voice(Arc::clone(&http_client), Arc::clone(&db))
                         .expect("Failed to initialize voice"),
-                    remind: remind::build_remind(Arc::clone(&db))?,
                 })
             })
         })
@@ -101,13 +97,6 @@ async fn event_handler(
     data: &Data,
 ) -> Result<(), Error> {
     match event {
-        serenity::FullEvent::Ready { data_about_bot: _ } => {
-            let remind_clone = data.remind.clone();
-            let ctx_clone = ctx.clone();
-            tokio::spawn(async move {
-                let _ = remind_clone.invoke_reminders(&ctx_clone).await;
-            });
-        }
         serenity::FullEvent::Message { new_message } => {
             data.voice.on_message(ctx, new_message).await?;
         }
